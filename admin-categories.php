45b7e3f0af909d8a7e64c12b34b547e6650872e7
Eduardo Dipp <eduardodipp7@gmail.com>
2019-07-25 13:13:19 -0300

    Admin Categorias - Busca e Paginação

--- admin-categories.php
+++ admin-categories.php
@@ -11,12 +11,45 @@ $app->get("/admin/categories/", function(){
 
 	User::verifyLogin();
 
+
+    $search = (isset($_GET['search'])) ? $_GET['search'] : "";
+    $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+    //metodo pra listar todos usuarios
+    //$users = User::listAll();//alterado na aula 126
+
+    if($search != ''){
+
+    	$pagination = Category::getPageSearch($search, $page);//criado esse novo
+
+    }else{
+
+    	$pagination = Category::getPage($page);//criado esse novo
+
+    }
+
+
+    $pages = [];
+
+    for($x = 0; $x < $pagination['pages']; $x++){
+
+    array_push($pages, [
+        'href'=>'/admin/categories/?'.http_build_query([
+         'page'=>$x+1,
+         'search'=>$search
+        ]),
+        'text'=>$x+1
+    ]);
+    }//fim do for
+
 	$categories = Category::listAll();
 
    $page  = new PageAdmin();
 
 	$page->setTpl("categories", [
-      'categories'=>$categories
+      'categories'=>$pagination['data'],
+    	'search'=>$search,
+    	'pages'=>$pages
 
 	]);
 
